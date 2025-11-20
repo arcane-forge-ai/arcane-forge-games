f327da9c0a3fe209e486a51d7a449b7027e69fee
BingBingBing <bing@bingai.studio>
2025-11-19 17:37:50 -0800

    fix(immune-rogue-preview): updated icons

--- public/game-library/immune-rogue-preview/flutter_bootstrap.js
+++ public/game-library/immune-rogue-preview/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "260794302"
+    serviceWorkerVersion: "3426240535"
   }
 });
